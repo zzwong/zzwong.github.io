fdbf18ce4467e49f26b6211641aafc5f6d00a398
Aaron Wong <aaronwong13@gmail.com>
2017-01-19 21:26:51 -0800

    exterminated semicolons and refactored declaration keyword const style

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 /**
  * Module dependencies
  */
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const passport = require('passport');
+const express = require('express')
+  ,bodyParser = require('body-parser')
+  ,path = require('path')
+  ,dotenv = require('dotenv')
+  ,mongoose = require('mongoose')
+  ,passport = require('passport')
 
 
 /**
  * Load environment keys and variables
  */
-dotenv.load({ path: '.env' });
+dotenv.load({ path: '.env' })
 
 /**
  * Controllers (route handlers).
  */
-const homeController = require('./controllers/home');
-//const blogController = require('./controllers/blog');
+const homeController = require('./controllers/home')
+//const blogController = require('./controllers/blog')
 
 /**
  * Passport configuration
@@ -28,52 +28,52 @@ const homeController = require('./controllers/home');
 /**
  * Create express server
  */
-const app = express();
+const app = express()
 
 /**
  * Connect to MongoDB.
  */
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI)
 mongoose.connection.on('error', () => {
-  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
-  process.exit(1);
-});
+  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.')
+  process.exit(1)
+})
 
 
 /**
  * express configs
  */
-app.set('port', 8080);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 8080)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public'), {maxAge: 31557600000}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: 31557600000}))
 
 
 /**
  * Primary app routes
  */
-app.get('/', homeController.index);
-app.get('/MLpaper', homeController.getPaper);
-app.get('/resume', homeController.getResume);
+app.get('/', homeController.index)
+app.get('/MLpaper', homeController.getPaper)
+app.get('/resume', homeController.getResume)
 
 
 /**
  * Blog routes
  */
-//app.get('/blog', blogController.getBlog);
-//app.post('/blog', blogController.postBlog);
-//app.post('/blog/admin', passportConfig.isAuthenticated, blogController.adminLogin);
-//app.get('/blog/title', blogController.getBlogByTitle); // Use title because its better for SEO
+//app.get('/blog', blogController.getBlog)
+//app.post('/blog', blogController.postBlog)
+//app.post('/blog/admin', passportConfig.isAuthenticated, blogController.adminLogin)
+//app.get('/blog/title', blogController.getBlogByTitle)  // Use title because its better for SEO
 
 // Handle tags
-//app.get('/blog/tag/', blogController.getBlog);
+//app.get('/blog/tag/', blogController.getBlog)
 
 
 
-app.listen(8080, ()=> {
-	console.log('express server listening on port 8080 ');
-});
-module.exports = app;
+app.listen(app.get('port'), () => {
+	console.log('express server listening on port 8080 ')
+})
+module.exports = app
--- test/test.js
+++ test/test.js
@@ -3,13 +3,13 @@
  * https://mochajs.org/
  */
 
-const app = require('../app.js');
-const request = require('supertest'); // used for HTTP testing
+const app = require('../app.js')
+  ,request = require('supertest')  // used for HTTP testing
 
 describe('GET /', () => {
   it('should return 200 OK', (done) => {
     request(app)
       .get('/')
-      .expect(200, done);
-  });
-});
+      .expect(200, done)
+  })
+})
